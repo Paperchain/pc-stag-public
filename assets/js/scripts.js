aaf67955d78bac17614bfe5753c1aa0ad3c7eed0
danielkdewar <mail@daniel-dewar.com>
2020-10-06 22:52:48 -0400

    latest website push

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -63,3 +63,26 @@
     $(window).scroll(navbarCollapse);
 
 })(jQuery);
+
+$(document).ready(function(){
+  $('.customer-logos').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 4
+      }
+    }, {
+      breakpoint: 520,
+      settings: {
+        slidesToShow: 3
+      }
+    }]
+  });
+});
